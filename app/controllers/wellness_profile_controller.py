a2a92f4a0b82370006dc3c727d76a1a98ca4dd82
Arnel Jan Sarmiento <arnel@advicerevolution.com.au>
2025-06-19 22:36:22 +0800

    feat: Update FastAPI application title and modify wellness profile route tag for clarity

--- app/controllers/wellness_profile_controller.py
+++ app/controllers/wellness_profile_controller.py
@@ -6,4 +6,4 @@ from app.controllers.wellness_profile_ws_routes import ws_routes
 
 def ws_controller(app: FastAPI):
     app.include_router(ws_routes, prefix='/ws', tags=['Websocket Endpoints'])
-    app.include_router(wellness_profile, prefix='/profile', tags=['Wellness Profile'])
+    app.include_router(wellness_profile, prefix='/profile', tags=['Wellness REST Profile'])
